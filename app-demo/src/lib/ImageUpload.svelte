67cc302a8123c72aea7d83836d600edba501af8d
ckim328 <christineskim@google.com>
2022-09-29 12:27:59 -0400

    Component for image upload

---
+++ app-demo/src/lib/ImageUpload.svelte
@@ -0,0 +1,43 @@
+<script>
+  let input;
+  let container;
+  let image;
+  let placeholder;
+	let showImage = false;
+
+  function onChange() {
+    const file = input.files[0];
+		
+    if (file) {
+			showImage = true;
+
+      const reader = new FileReader();
+      reader.addEventListener("load", function () {
+        image.setAttribute("src", reader.result);
+      });
+      reader.readAsDataURL(file);
+			
+			return;
+    } 
+		showImage = false; 
+  }
+
+
+</script>
+
+
+<input
+	bind:this={input}
+	on:change={onChange}
+  type="file"
+/>
+<div bind:this={container}>
+	{#if showImage}
+		<img bind:this={image} src="" alt="Preview" />
+	{:else}
+		<span bind:this={placeholder}>Image Preview</span>
+	{/if}
+</div>
+<!-- <button on:click={increment}>
+  count is {count}
+</button> -->
